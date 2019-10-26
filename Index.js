ce22adb19432d80c34d450fe08e05527d9d861ed
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-25 23:59:46 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,2 +1,26 @@
-const language = require("@google-cloud/language");
-const places = require("@google/maps/lib/places");
+//const language = require("@google-cloud/language");
+const places = require("@google/maps");
+const port = process.env.PORT;
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
+  
+    // Instantiates a client
+    const client = new language.LanguageServiceClient();
+  
+    // The text to analyze
+    const text = 'Hello, world!';
+  
+    const document = {
+      content: text,
+      type: 'PLAIN_TEXT',
+    };
+  
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeSentiment({document: document});
+    const sentiment = result.documentSentiment;
+  
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  }
