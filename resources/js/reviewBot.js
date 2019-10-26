4ef625ae185a0dc678580b30664487067d7d1005
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-26 19:00:40 -0400

    Update reviewBot.js

--- resources/js/reviewBot.js
+++ resources/js/reviewBot.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/NL", (req, res) => {
-  // Imports the Google Cloud client library
-  const language = require("@google-cloud/language");
+// Imports the Google Cloud client library
+const language = require("@google-cloud/language");
+// Instantiates a client
+const client = new language.LanguageServiceClient();
 
-  // Instantiates a client
-  const client = new language.LanguageServiceClient();
+router.post("/sentiment", (req, res) => {
 
   const result = req.body;
   const sentiment = [];
@@ -23,9 +23,34 @@ router.post("/NL", (req, res) => {
   }
 
   Promise.all(sentiment).then(results => {
-      for(var i=0; i<results.length;i++){
-          response.push(results[i][0].documentSentiment)
-      }
+    for (var i = 0; i < results.length; i++) {
+      response.push(results[i][0].documentSentiment);
+    }
+    return res.status(200).send(response);
+  });
+});
+
+
+//Analyze entities
+router.post("/entities", (req, res) => {
+  const result = req.body;
+  const sentiment = [];
+  const response = [];
+
+  for (var i = 0; i < result.length; i++) {
+    // The text to analyze
+    const document = {
+      content: result[i].text,
+      type: "PLAIN_TEXT"
+    };
+
+    sentiment.push(client.analyzeEntities({ document: document }));
+  }
+
+  Promise.all(sentiment).then(results => {
+    for (var i = 0; i < results.length; i++) {
+      response.push(results[i][0].documentSentiment);
+    }
     return res.status(200).send(response);
   });
 });
