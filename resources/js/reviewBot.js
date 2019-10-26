f2c6fe2b51db8818bd815b62716dc40c3b3814d2
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-26 13:49:44 -0400

    Update reviewBot.js

--- resources/js/reviewBot.js
+++ resources/js/reviewBot.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const language = require('@google-cloud/language');
 
-router.get("/NL", (req, res) =>{
-        res.send("magic");
-})
-    
-  
+router.get("/NL", (req, res) => {
+  // Imports the Google Cloud client library
+  const language = require("@google-cloud/language");
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  // The text to analyze
+  const text = "Hello, world!";
+
+  const document = {
+    content: text,
+    type: "PLAIN_TEXT"
+  };
+
+  // Detects the sentiment of the text
+  client
+    .analyzeSentiment({ document: document })
+    .then(results => {
+      const sentiment = results[0].documentSentiment;
+      return res.status(200).send(`Text: ${text} Sentiment score: ${sentiment.score} Sentiment magnitude: ${sentiment.magnitude}`);
+    })
+    .catch(err => {
+      return res.send(err);
+    });
+
+});
 
 module.exports = router;
