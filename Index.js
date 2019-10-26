4e8e26effe84af8c1f035c9a58f9b98faec94fab
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-26 11:13:13 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,15 +1,22 @@
+//Libraries
 const express = require("express");
 const cors = require("cors");
-const reviewBot = require("./resources/js/reviewBot");
-const myApp = express();
+const reviewBot = require("./resources/js/reviewBot.js");
+const bodyParser = require("body-parser");
 
+//Server info
+const myApp = express();
 const port = 8000;
 const hostname = '127.0.0.1';
 
-myApp.use(cors);
-myApp.use(express.json());
-myApp.use('/resources/reviewBot.js', reviewBot);
+//Middleware
+myApp.use(cors());
+myApp.use(bodyParser.json());
 
-myApp.listen(port, ()=>{
+//Routes
+myApp.use('/reviewBot', reviewBot);
+
+//Hosting
+myApp.listen(port, hostname, ()=>{
   console.log(`Server running at http://${hostname}:${port}/`);
 })
