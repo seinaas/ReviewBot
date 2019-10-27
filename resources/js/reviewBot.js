7952d85686f9f930c8f89b84af967ee214e05fc0
AdamMigliore <33816654+AdamMigliore@users.noreply.github.com>
2019-10-27 01:04:07 -0400

    Update reviewBot.js

--- resources/js/reviewBot.js
+++ resources/js/reviewBot.js
@@ -34,28 +34,6 @@ const brocheAfoin = [];
   });
 });
 
-//Analyze entities
-router.post("/entities", (req, res) => {
-  const result = req.body;
-  const sentiment = [];
-  const response = [];
 
-  for (var i = 0; i < result.length; i++) {
-    // The text to analyze
-    const document = {
-      content: result[i].text,
-      type: "PLAIN_TEXT"
-    };
-
-    sentiment.push(client.analyzeEntities({ document: document }));
-  }
-
-  Promise.all(sentiment).then(results => {
-    for (var i = 0; i < results.length; i++) {
-      response.push(results[i][0].documentSentiment);
-    }
-    return res.status(200).send(response);
-  });
-});
 
 module.exports = router;
